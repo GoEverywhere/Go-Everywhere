9ed787e662600da34cc72bde6d004ad050a1c534
axxonyara <asreg123@gmail.com>
2013-07-21 12:13:54 -0400

    Fixed download script and added friendly admin  page.

--- admin/download-github.php
+++ admin/download-github.php
@@ -1,13 +1,13 @@
 <?php
-/* This script will download the current zip archive from Github to whatever server we are on at the moment
+/* This script will download the current zip archive from GitHub to whatever server we are on at the moment
  * Then, extract it into the main directory, as to update the site
  */
-//downlaod the zip from Github
-file_put_contents("goev-tmp.zip", fopen("https://github.com/GoEverywhere/Go-Everywhere/archive/master.zip", 'r'));
-//now, just to unzip it!
-//create the ZipArchive object
+// Download the zip from Github
+file_put_contents("goev-tmp.zip", file_get_contents("http://github.com/GoEverywhere/Go-Everywhere/archive/master.zip"));
+// Now, just to unzip it!
+// Create the ZipArchive object
 $zip = new ZipArchive;
-//open the zip
+// Open the zip
 $res = $zip->open('goev-tmp.zip');
 if ($res === TRUE) {
     //extract it to the current directory (for now, to see if this will all work)
@@ -16,7 +16,7 @@ if ($res === TRUE) {
     $zip->close();
     //we are all done. Hooray!
     //Now, we go to a new script to finish the process
-    header("../../rename.php");
+    header("Location: rename.php");
 } else {
     //Well, it failed
     echo 'Download was OK, but the extraction failed. Go check the logs and see why.';
