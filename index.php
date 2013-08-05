0e21a2fb9c0cdf77e4eb72551d3b34806e955d42
Philip Rader <ifelloffofatree@gmail.com>
2013-08-04 22:55:56 -0700

    Moved files around for better organization
    Reference header.php and footer.php from the includes folder

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //include the header (but make sure it is only once, to not redo any code that shouldn't)
-include_once("./header.php");
+include_once("./includes/header.php");
 ?>
 <p>Go Everywhere is inspired by Scratch, to edit Scratch projects on mobile devices, via HTML5, and community made. Each supported device will have a specific stylesheet, making the interface more user-friendly. And for the devices that work, but are not specifically supported, a general stylesheet will be created, as temporary support for the device until we can get a stylesheet working.</p>
 <h1><u>Why should I care? The Scratch Team is already working (or is going to work) on Scratch 2.0 in HTML5.</u></h1>
@@ -9,5 +9,5 @@ include_once("./header.php");
 <p>Thanks for the offer! We can use the extra help. Before, we all had certain jobs, and were committed to getting them done. Some of us left, and some of us didn't finish our jobs. However, looking around, I noticed that this was not the best option for a community. I was looking, and found MonoGame. MonoGame is a great community project, because anybody can participate. There is no commitment, just do it when you feel like it. So, this is how it is going to work. You must have a <a href="http://github.com/" target="_blank">GitHub</a> account, as we will be using GitHub. You also must fork our main repository (me included, as so I don't mess anything up in the main branch). In your fork, edit and add any features you wish to contribute. Then, submit a pull request. We will merge it into our "Experimental Branch", and see if it will be compatible. If it is, we will merge our "Experimental Branch" into the "Master" branch. If somebody already has submitted a pull request that you wanted to work on, by all means, work on it! If yours is more compatible, or is better, smarter code, we will more likely use it!</p>
 <?php
 //include the footer
-include_once("./footer.php");
+include_once("./includes/footer.php");
 ?>
