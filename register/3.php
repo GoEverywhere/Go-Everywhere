3cf27263b346d97ba894b7772f8b88b0dbcd7e9c
Philip Rader <ifelloffofatree@gmail.com>
2013-08-07 09:54:41 -0700

    Perfecting register system with Scratch verification
    credit to jvvg for letting us use his system

--- register/3.php
+++ register/3.php
@@ -5,19 +5,19 @@
 //start the session
 session_start();
 //variables for later (credit to jvvg)
-$project_id = '10135908';
+$project_id = '10135908/';
 $project_url = 'http://scratch.mit.edu/projects/' . $project_id;
-$api_url = 'http://scratch.mnit.edu/site-api/comments/project/' . $project_id . '?page1&salt=' . md5(time()); //salt is to prevent caching
+$api_url = 'http://scratch.mit.edu/site-api/comments/project/' . $project_id . '?page=1&salt=' . md5(time()); //salt is to prevent caching
 //check to see if the user is still in the session
-if(empty($_SESSION['tmp_username']))
+if(empty($_SESSION['tmp_username']) || $_SESSION['tmp_username'] == "")
 {
 	//We need to send them back to step one
-	header('Location: 1.php');
+	header('Location: ./1.php');
 }else{
 	if(empty($_SESSION['tmp_user_code']))
 	{
 		//We need to send them back to step two
-		header('Location: 2.php');
+		header('Location: ./2.php');
 	}else{
 		//Check to verify that the user commented on the Scratch project (credit to jvvg)
 		$data = file_get_contents($api_url);
@@ -29,12 +29,14 @@ if(empty($_SESSION['tmp_username']))
 		preg_match_all('%<div id="comments-\d+" class="comment.*?" data-comment-id="\d+">.*?<a href="/users/(.*?)">.*?<div class="content">(.*?)</div>%ms', $data, $matches);
 		foreach ($matches[2] as $key => $val)
 		{
-			$user = $matches[1][$key};
+			$user = $matches[1][$key];
 			$comment = trim($val);
 			if($user == $_SESSION['tmp_username'] && $comment == $_SESSION['tmp_user_code'])
 			{
 				$success = true;
 				$_SESSION['verifieduser'] = $_SESSION['tmp_username'];
+				//include the header
+				include_once('../includes/header.php');
 				?>
 					<h2>Signup! ~Step 3</h2>
 					<?php
@@ -42,6 +44,7 @@ if(empty($_SESSION['tmp_username']))
 					{
 					?>
 					<h3 style="color:red;">Please make sure that your passwords match!</h3>
+					<?php } ?>
 					<p>Please insert a password for your Go Everywhere account for <span class="color:orange;"><?php echo $_SESSION['verifieduser']; ?></span>.</p>
 					<form style="padding: 5px;border: 1px solid black; width: 450px;" method="post" action="./4.php" enctype="application/x-www-form-urlencoded">
     						<label for="password">Go Everywhere Password: </label>
@@ -51,12 +54,14 @@ if(empty($_SESSION['tmp_username']))
     						<input type="submit" value="Signup!" />
 					</form>
 				<?php
+				//include the footer
+				include_once('../includes/footer.php');
 			}
-		}
-		if(!$success)
-		{
-			//Didn't find it. Send the user to the last step
-			header('Location: ./2.php');
+			if(!$success)
+			{
+				//Didn't find it. Send the user to the last step
+				header('Location: ./2.php');
+			}
 		}
 	}
 }
--- register/4.php
+++ register/4.php
@@ -1,5 +1,7 @@
 <?php
 /* This script takes the verified user, and inserts them into the database */
+//start the session
+session_start();
 //get the database info
 require_once("../../db_constants.php");
 //see if all the required info is here
@@ -9,7 +11,7 @@ if(isset($_SESSION['username']))
 	header('Location: ../');
 	exit;
 }
-if(empty($_SESSION['verifieduser']))
+if(empty($_SESSION['verifieduser']) || $_SESSION['verifieduser'] == "")
 {
 	//They are not verified. We should send them back to step one, and start over
 	header('Location: ./1.php');
@@ -21,7 +23,7 @@ if(empty($_POST['password']) || $_POST['password'] == "" || empty($_POST['passwo
 	header('Location: ./3.php');
 	exit;
 }
-if($_POST['password'] == $_POST['password2'])
+if($_POST['password'] != $_POST['password2'])
 {
 	//They didn't fill out the last form correctly!
 	header('Location: ./3.php?error=yes');
@@ -30,28 +32,28 @@ if($_POST['password'] == $_POST['password2'])
 //Okay, now we can begin to register them!
 db_connect();
 //See if they are already registered
-$result = mysql_query("SELECT * FROM users WHERE user='" . $_POST['username'] . "'");
+$result = mysql_query("SELECT * FROM users WHERE user='" . $_SESSION['verifieduser'] . "'");
 if(mysql_num_rows($result) > 0)
 {
     	//They are already registered!
-    	$errormessage = "ERROR: You are already registered! Go log in instead!";
+    	echo "<p>ERROR: You are already registered! Go log in instead!</p>";
 }else{
-	//Insert them into the database!
-	db_query("INSERT INTO users VALUES('" . $_POST['username'] . "', '" . sha1($_POST['password']) . "', 1, 1, 0)");
-	//Log the user in
-	//set user in the session
-	$_SESSION ['username'] = $username;
-	//set a cookie to the user
-	//this can be improved even more, but I'm not to worried about it right now,
-	//since it isn't so popular that people would hack users. But, we will improve on
-	//security soon
-	setCookie('user_id', $username);
-	//unset all the other SESSION variables we had
-	unset($_SESSION['verifieduser']);
-	unset($_SESSION['tmp_username']);
-	unset($_SESSION['tmp_user_code']);
-	//send them on their way!
-	header('Location: ./index.php');
-	}
+    //Insert them into the database!
+    db_query("INSERT INTO users VALUES('" . $_SESSION['verifieduser'] . "', '" . sha1($_POST['password']) . "', 1, 1, 0)");
+    //Log the user in
+    session_start();
+    //set user in the session
+    $_SESSION ['username'] = $username;
+    //set a cookie to the user
+    //this can be improved even more, but I'm not to worried about it right now,
+    //since it isn't so popular that people would hack users. But, we will improve on
+    //security soon
+    setCookie('user_id', $username);
+    //unset all the other SESSION variables we had
+    unset($_SESSION['verifieduser']);
+    unset($_SESSION['tmp_username']);
+    unset($_SESSION['tmp_user_code']);
+    //send them on their way!
+    header('Location: ../?verified=1');
 }
 ?>
