f8c94fbdfb57ba4d64f627e54f49fef739544e13
Philip Rader <ifelloffofatree@gmail.com>
2013-08-15 19:12:28 -0700

    Preperation for profile page

--- profile/index.php
+++ profile/index.php
@@ -1,5 +1,29 @@
 <?php
 include_once("../includes/header.php");
+//include database info
+require_once("../../db_constants.php");
+//connect to the database
+db_connect();
+//see if we need to view a certain user, or the curren't user's profile page
+$displayOwn = true;
+if(isset($_GET['user']) && $_GET['user'] != "")
+{
+    $displayOwn = false;
+}
+$user = $_SESSION['username'];
+if($displayOwn)
+{
+    $user = $_GET['user'];
+}
+//query the user
+$result = mysql_query("SELECT * FROM users WHERE user='" . mysql_real_escape_string($user) . "'") or die(mysql_error());
+if(mysql_num_rows($result) == 0)
+{
+    //Error. No user. Should replace this with an error 404 page, when we get one
+    echo "Error: No such user";
+    exit;
+}
+$info = mysql_fetch_array($result);
 ?>
 
 <!-- Someone add a profile picture thing and password change stuffs -->
