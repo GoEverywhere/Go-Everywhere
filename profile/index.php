16512d26af1b01a96444d9128215f9b0e313a255
lolzballs <lolzballs@teamfield3.tk>
2013-08-15 20:43:58 -0400

    Fixed include locations.

--- profile/index.php
+++ profile/index.php
@@ -1,5 +1,5 @@
 <?php
-include_once("./includes/header.php");
+include_once("../includes/header.php");
 ?>
 
 <!-- Someone add a profile picture thing and password change stuffs -->
@@ -7,5 +7,5 @@ include_once("./includes/header.php");
 <h1><u>Under Construction</u></h1>
 
 <?php
-include_once("./includes/footer.php");
+include_once("../includes/footer.php");
 ?>
