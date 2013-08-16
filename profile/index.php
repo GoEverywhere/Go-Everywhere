a0d8cd16ea927839221e5fd2458200af82f9ce81
lolzballs <lolzballs@teamfield3.tk>
2013-08-15 20:40:58 -0400

    Added a profile page

---
+++ profile/index.php
@@ -0,0 +1,11 @@
+<?php
+include_once("./includes/header.php");
+?>
+
+<!-- Someone add a profile picture thing and password change stuffs -->
+<!-- I'll find the time to do this later if you don't want to -->
+<h1><u>Under Construction</u></h1>
+
+<?php
+include_once("./includes/footer.php");
+?>
