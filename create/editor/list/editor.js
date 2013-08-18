a8e510081206dfe75747157e5cfb29afa657ac44
Philip Rader <ifelloffofatree@gmail.com>
2013-08-18 16:39:11 -0700

    Able to load Scratch 2.0 files

--- create/editor/list/editor.js
+++ create/editor/list/editor.js
@@ -1,3 +1,6 @@
+//VARIABLES
+var zipFile;
+
 function reloadEvents() {
     $("#blocks .sortable-script").sortable({
         axis: "y"
@@ -5,4 +8,20 @@ function reloadEvents() {
 }
 $(document).ready(function(){
     reloadEvents();
+    loadProject("1.ge");
 });
+function loadProject(url) {
+    zipFile = new ZipFile(url, doneReadingZip, 1);
+    
+}
+function doneReadingZip(zip) {
+    var project;
+    //Load the project JSON into the project variable
+    for (i=0;i<zipFile.entries.length;i++) {
+	if (zipFile.entries[i].name == "project.json") {
+	    project = JSON.parse(zipFile.entries[i].extract(null, true));
+	}
+    }
+    //DEBUG!
+    window.alert(project.objName);
+}
