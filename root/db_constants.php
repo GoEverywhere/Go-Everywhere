6e3dd571e35fc4b17dfc6c9c786265ffce66e65e
Philip Rader <ifelloffofatree@gmail.com>
2014-01-04 17:26:12 -0800

    Getting things ready for PostgreSQL switch

--- root/db_constants.php
+++ root/db_constants.php
@@ -11,45 +11,92 @@
  * equivalent. I do not have the time to learn mysqli, so i am going to
  * continue writing this in mysql functions. PLEASE REMOVE THIS NOTICE IF
  * YOU CHANGE TO MYSQLI!!!!!!!!!!!!!
+ *
+ * Please note that the GE server uses PostgreSQL. You will have a choice to use
+ * PostgreSQL or MySQL. If MySQL, make sure USING_POSTGRESQL is set to false, and
+ * change the MYSQL_* info to your server's info.
+ * If PostgreSQL, change USING_POSTGRESQL to true, and change the POSTGRESQL_*
+ * to your server's info.
  */
+define("USING_POSTGRESQL", false);
+ //MYSQL Info
 define("MYSQL_USER", "root");
 define("MYSQL_PASSWORD", "");
 define("MYSQL_SERVER", "127.0.0.1");
 define("MYSQL_DATABASE", "Go-Everywhere");
+//POSTGRESQL Info
+define("POSTGRESQL_USER", "root");
+define("POSTGRESQL_PASSWORD", "");
+define("POSTGRESQL_SERVER", "127.0.0.1");
+define("POSTGRESQL_DATABASE", "Go-Everywhere");
+
 //The salt for crypting users (change to be more secure)
 define("LOGIN_SALT", "goev");
 
+//A constant defining read-only mode (no inserting data)
+define("GE_READ_ONLY", false);
+
 function db_connect()
 {
-    //Call this function whenever you want to connect to the database
-    //Connect to MySQL, die any errors (for now)
-    mysql_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD) or die ('Could not connect to MySQL: ' . mysql_error());
-    //Connect to the database (a script for a default database setup, in an sql file, will be put on our site)
-    mysql_select_db(MYSQL_DATABASE) or die ('Could not select database: ' . mysql_error());
+    if(!USING_POSTGRESQL)
+    {
+        //Call this function whenever you want to connect to the database
+        //Connect to MySQL, die any errors (for now)
+        return mysql_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD) or die ('Could not connect to MySQL: ' . mysql_error());
+        //Connect to the database (a script for a default database setup, in an sql file, will be put on our site)
+        return mysql_select_db(MYSQL_DATABASE) or die ('Could not select database: ' . mysql_error());
+    }else{
+        return pg_connect("host=" . POSTGRE_SERVER . " dbname=" . POSTGRESQL_DATABASE . " user=" . POSTGRESQL_USER . " password=" . POSTGRESQL_PASSWORD . "") or die('Could not connect to PostgreSQL: ' . pg_last_error());
+    }
 }
 function db_query($q)
 {
-    //Call this function whenever you want to query the database
-    return mysql_query($q);
+    if(!USING_POSTGRESQL)
+    {
+        //Call this function whenever you want to query the database
+        return mysql_query($q);
+    }else{
+        return pg_query($q);
+    }
 }
 function db_fetch_array($result)
 {
-    //Call this function whenever you want to get a row array from the query result
-    return mysql_fetch_array($result);
+    if(!USING_POSTGRESQL)
+    {
+        //Call this function whenever you want to get a row array from the query result
+        return mysql_fetch_array($result);
+    }else{
+        return pg_fetch_array($result);
+    }
 }
 function db_num_rows($result)
 {
-    //Call this function whenever you want to get the number of does returned from the query result
-    return mysql_num_rows($result);
+    if(!USING_POSTGRESQL)
+    {
+        //Call this function whenever you want to get the number of does returned from the query result
+        return mysql_num_rows($result);
+    }else{
+        return pg_num_rows($result);
+    }
 }
 function db_free_result($result)
 {
-    //Call this function whenever you want to free your result
-    return mysql_free_result($result);
+    if(!USING_POSTGRESQL)
+    {
+        //Call this function whenever you want to free your result
+        return mysql_free_result($result);
+    }else{
+        return pg_free_result($result);
+    }
 }
 function db_disconnect()
 {
-    //Call this function once you are done querying MySQL
-    //There might be something extra we need to do, so just call this to be up to date
-    mysql_close();
+    if(!USING_POSTGRESQL)
+    {
+        //Call this function once you are done querying MySQL
+        //There might be something extra we need to do, so just call this to be up to date
+        return mysql_close();
+    }else{
+        return pg_close();
+    }
 }
