22b4a67df86878656bf98dd6b96f5c3168d8c235
Philip Rader <ifelloffofatree@gmail.com>
2014-05-13 16:36:44 -0700

    Errors in node-webkit running in Windows
    Kept closing the node-webkit Window. Also added extra error catches, for
    better debugging, and waiting for the file to completely load before
    loading project.

--- create/editor/list/editor.js
+++ create/editor/list/editor.js
@@ -395,34 +395,36 @@ $(document).ready(function(){
     
     //IO (with node.js)
     if (usingnw || usinggap) {
-	//We don't need this here
-	$("#saveProject").remove();
-	if (usingnw) {
-	    //We are in nw mode, so there is a project waiting (is required, either by user or default)
-	    doneReadingZip();
-	}else if (usinggap) {
-	    //Show the Android tools
-	    $("#androidTools").show();
-	    //Bind the "open" to the file browser
-	    $("#androidTools #open").bind("touchstart", function(){
-		
-	    });
-	    //We are in gap, so we need to load the file here (we own the function)
-	    loadProject("../default.ge");
-	}
-	
+		//We don't need this here
+		$("#saveProject").remove();
+		if (usingnw) {
+			//We are in nw mode, so there is a project waiting (is required, either by user or default)
+			$(document).one("geprojectload", function(){
+				doneReadingZip();
+			});
+			//loadProject("../default.ge");
+		}else if (usinggap) {
+			//Show the Android tools
+			$("#androidTools").show();
+			//Bind the "open" to the file browser
+			$("#androidTools #open").bind("touchstart", function(){
+			
+			});
+			//We are in gap, so we need to load the file here (we own the function)
+			loadProject("../default.ge");
+		}
     }
     //IO (with not using node.js)
     if (!usingnw && !usinggap) {
-	$("#saveProject").bind("click touchend", function(e){
-	    generateAndDownloadZIP();
-	});
-	if (vars['project'] && vars['project'] != "") {
-	    //load project
-	    loadProject(vars['project']);
-	}else{
-	    $("#blocks").html("<br /><br /><h3>Please provide a project in ?project=PROJECT_PATH</h3>");
-	}
+		$("#saveProject").bind("click touchend", function(e){
+			generateAndDownloadZIP();
+		});
+		if (vars['project'] && vars['project'] != "") {
+			//load project
+			loadProject(vars['project']);
+		}else{
+			$("#blocks").html("<br /><br /><h3>Please provide a project in ?project=PROJECT_PATH</h3>");
+		}
     }
 });
 function loadProject(url) {
